655649aef85e6729f7e6a40907ed5fac32469ed4
Ignacio Mariani <ignaciom6@hotmail.com>
2020-03-29 14:25:16 -0300

    Model created

---
+++ BTCityFinderSwift/Model/BTCityCoordinatesDataModel.swift
@@ -0,0 +1,9 @@
+//
+//  BTCityCoordinatesDataModel.swift
+//  BTCityFinderSwift
+//
+//  Created by Ignacio on 28/02/2020.
+//  Copyright © 2020 Ignacio. All rights reserved.
+//
+
+import Foundation
---
+++ BTCityFinderSwift/ViewControllers/BTCityListViewController.swift
@@ -0,0 +1,9 @@
+//
+//  BTCityListViewController.swift
+//  BTCityFinderSwift
+//
+//  Created by Ignacio on 28/02/2020.
+//  Copyright © 2020 Ignacio. All rights reserved.
+//
+
+import Foundation
